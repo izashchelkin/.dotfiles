ce5628989a328c152b1615d6dab84b399da69558
Ihor Zashchelkin <me@ihorzash.com>
2026-04-12 15:28:11 +0200

    a

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -28,3 +28,53 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
   nested = true,
 })
 
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = { "*.cc", "*.c", "*.cpp", "*.h", "*.hpp" },
+  callback = function()
+    vim.cmd("LspStart clangd")
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  pattern = { "*.cc", "*.c", "*.cpp", "*.h", "*.hpp" },
+  callback = function(args)
+    local bufnr = args.buf
+    vim.schedule(function()
+      if vim.api.nvim_buf_is_valid(bufnr) and #vim.fn.win_findbuf(bufnr) == 0 then
+        for _, client in ipairs(vim.lsp.get_clients({ bufnr = bufnr, name = "clangd" })) do
+          vim.lsp.buf_detach_client(bufnr, client.id)
+        end
+      end
+    end)
+  end,
+})
+
+vim.api.nvim_create_autocmd("FocusLost", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("silent! LspStop clangd")
+  end,
+})
+
+vim.api.nvim_create_autocmd("FocusGained", {
+  pattern = "*",
+  callback = function()
+    local visible_bufs = {}
+    
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      visible_bufs[vim.api.nvim_win_get_buf(win)] = true
+    end
+
+    for bufnr, _ in pairs(visible_bufs) do
+      if vim.api.nvim_buf_is_valid(bufnr) then
+        local ft = vim.bo[bufnr].filetype
+        if ft == "c" or ft == "cpp" or ft == "objc" or ft == "objcpp" then
+          vim.api.nvim_buf_call(bufnr, function()
+            vim.cmd("silent! LspStart clangd")
+          end)
+        end
+      end
+    end
+  end,
+})
+
